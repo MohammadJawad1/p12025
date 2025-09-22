4575df229559bbc9304ad2114b893ec56863749a
MohammmadJawad1 <mohammad.jawad30@myhunter.cuny.edu>
2025-09-22 18:55:56 -0400

    param

--- File.cpp
+++ File.cpp
@@ -6,6 +6,23 @@
 
 File::File() : filename_("New_Text_Document.txt"), readable_(true), writable_(true), last_modified_timestamp_(cr::system_clock::now()){}
 
+/**
+* Constructs a new File object.
+*
+* @param filename A const reference to a string for the initial filename
+* @param isReadable A const reference to a boolean for the readable permission
+* @param isWritable A const reference to a boolean for the writable permission
+*
+* @post
+* All booleans should have a default value of true (ie. use true if they are omitted).
+*
+* Sets the File's filename_ such that:
+* 1. If the parameter contains spaces or has no characters at all, the default filename is used
+* 2. If the parameter has no extension (no `period` characters whatsoever), .txt is appended to the filename & used
+* 3. If the parameter has an empty extension (a `period` characters followed by no characters), txt is appended to the filename & used
+* 4. Otherwise, the parameter filename is used as is
+* Note: A filename like `.env` or `.log` IS allowed (ie. filenames containing only the extension)!
+*/
 File::File(const std::string& filename, bool isReadable, bool isWritable) : readable_(isReadable), writable_(isWritable), last_modified_timestamp_(cr::system_clock::now()) 
 {
     std::string defaultName = "New_Text_Document.txt";
@@ -24,7 +41,7 @@ File::File(const std::string& filename, bool isReadable, bool isWritable) : read
             }
             else if (pos == filename.length() - 1)
             {
-                filename_ = filename + ".txt";
+                filename_ = filename + "txt";
             }
             else
             {
